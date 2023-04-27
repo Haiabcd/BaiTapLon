8f4cac787f4ad67cb5ce76c8f24ce93e81332020
Knguyen2003 <nguyenphan1122k3gmail.com>
2023-04-28 02:14:49 +0700

    edit BaiTapLon

---
+++ JS/pageHome.js
@@ -0,0 +1,10 @@
+var index =1;
+changeImage = function (){
+    var imgs = ["/IMAGES/slider_1.webp","/IMAGES/slider_2.webp","/IMAGES/slider_3.webp","/IMAGES/slider_4.webp"];
+    document.getElementById('img').src =imgs[index];
+    index++;
+    if(index==4){
+        index=0;
+    }
+}
+setInterval(changeImage,1000);
