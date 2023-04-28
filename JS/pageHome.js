b4f074e63026f9fa1228554ec38566b5e3c627ea
ANHHAI <haianhchidan@gmail.com>
2023-04-28 13:45:28 +0700

    update lan2

--- JS/pageHome.js
+++ JS/pageHome.js
@@ -1,6 +1,6 @@
 var index =1;
 changeImage = function (){
-    var imgs = ["/IMAGES/slider_1.webp","/IMAGES/slider_2.webp","/IMAGES/slider_3.webp","/IMAGES/slider_4.webp","/IMAGES/slider_5.webp"];
+    var imgs = ["../IMAGES/slider_1.webp","../IMAGES/slider_2.webp","../IMAGES/slider_3.webp","../IMAGES/slider_4.webp","../IMAGES/slider_5.webp"];
     document.getElementById('img').src =imgs[index];
     index++;
     if(index==5){
